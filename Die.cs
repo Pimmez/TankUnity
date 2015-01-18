e57f5186ada55d594fb4c4bd6da7bac14298257b
Pimmez <Pimmez@users.noreply.github.com>
2015-01-18 23:51:24 +0100

    Create Die.cs

---
+++ Die.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class Die : MonoBehaviour {
+
+	public int lives = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision coll)//OnTriggerEnter
+	{
+		//print("what hit me?" + coll.gameObject.name);
+		if (coll.gameObject.name == "Bullet(Clone)") 
+		{
+			lives--;
+			if(lives <= 0)
+			{
+				Destroy(this.gameObject);
+			}
+
+		}
+	}
+}
