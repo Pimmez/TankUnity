95bd8ce1cb6d21c866caea045808de4f896f38e3
Pimmez <Pimmez@users.noreply.github.com>
2015-01-18 23:51:47 +0100

    Create MoveBullet.cs

---
+++ MoveBullet.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+public class MoveBullet : MonoBehaviour {
+	public float speed;
+	private float lifeTime = 0f;
+	public float maxLifeTime;
+	public GameObject explosionPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		float delta = Time.deltaTime;
+		transform.Translate (Vector3.forward * speed * delta);
+		lifeTime += delta;
+		if (lifeTime > maxLifeTime)
+		{
+			Destroy(this.gameObject);
+		}
+
+	}
+	void OnCollisionEnter(Collision coll)
+	{
+		Instantiate (explosionPrefab, this.transform.position, this.transform.rotation);
+		Destroy (this.gameObject);
+
+
+	}
+}
