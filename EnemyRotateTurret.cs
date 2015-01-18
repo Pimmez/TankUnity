95f52ec1ab0744364f6caae2c53cd51580c42aa3
Pimmez <Pimmez@users.noreply.github.com>
2015-01-18 23:56:51 +0100

    Create EnemyRotateTurret.cs

---
+++ EnemyRotateTurret.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyRotateTurret : BaseRotateTurret {
+	public Transform player;
+	
+	// Update is called once per frame
+	override protected void Update () {
+
+		if (player != null) 
+		{
+			targetPos = player.position;
+			base.Update ();
+		}
+
+	}
+}
